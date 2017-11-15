4fa9cd89bee6f2e0bc8c2267a7452fdafa2edd25
lneoncyanide <mafer.9814@gmail.com>
2017-11-14 19:24:57 -0800

    Add Navigtion Scene

---
+++ TsunSim/Assets/Scripts/Next.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FeatureManager : MonoBehaviour {
+
+	private GameObject[] characterList;
+
+
+	// Use this for initialization
+	void Start () {
+		characterList = new GameObject[transform.childCount];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
+	
