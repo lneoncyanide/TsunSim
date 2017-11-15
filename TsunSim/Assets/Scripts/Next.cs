df1141cc3a5c8af548986fb972ebb799acf330b1
lneoncyanide <mafer.9814@gmail.com>
2017-11-14 19:37:45 -0800

    Add Next Script

--- TsunSim/Assets/Scripts/Next.cs
+++ TsunSim/Assets/Scripts/Next.cs
@@ -10,6 +10,11 @@ public class FeatureManager : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		characterList = new GameObject[transform.childCount];
+
+		for(int i = 0; i < transform.childCount; i++)
+		{
+			characterList [i] = transform.GetChild (i).gameObject;
+		
 	}
 	
 	// Update is called once per frame
