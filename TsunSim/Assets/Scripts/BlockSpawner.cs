4b964545c8be4efe73009aab470896b248006a68
DankDinis <33271900+DankDinis@users.noreply.github.com>
2017-11-21 19:32:42 -0800

    Obstacles

---
+++ TsunSim/Assets/Scripts/BlockSpawner.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockSpawner : MonoBehaviour
+{
+
+
+    public float spawnTimer = 2f;
+    public float spawnDelay = 1f;
+    public GameObject ObstaclePrefab;
+    public float SpawnX = 10f;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time >= spawnTimer)
+        {
+            SpawnObstacles();
+            spawnTimer = Time.time + spawnDelay;
+
+        }
+    }
+    void SpawnObstacles()
+    {
+        int random = Random.Range(-9, 10);
+        Vector2 randomCoordinates = new Vector2(SpawnX, random);
+        Instantiate(ObstaclePrefab, randomCoordinates, Quaternion.identity);
+    }
+}
---
+++ TsunSim/Assets/Scripts/Obstacles.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacles : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float gameSpeed = 15f;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector2 Move = new Vector2(-gameSpeed * Time.deltaTime, 0);
+
+        rb.MovePosition(rb.position + Move);
+        if (rb.position.x < -20)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
