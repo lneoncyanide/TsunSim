c1f23a604800f9e07a3727b2b88cc4a8e3a5343f
DankDinis <33271900+DankDinis@users.noreply.github.com>
2017-11-28 19:58:54 -0800

    The good Tstuff

---
+++ TsunSim/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Touch[] x = Input.touches;
+        Debug.Log(x + "!");
+        if(x.GetLength(0) >= 1)
+        {
+            Debug.Log("Dope");
+        }
+	}
+}
