0ee44e814508cd44731809e0ba47da667dc4dbca
DragonPickles <jennyrodriguez843@gmail.com>
2017-11-28 19:31:35 -0800

    main menu

--- TsunSim/Assets/Scripts/main_menu.cs
+++ TsunSim/Assets/Scripts/main_menu.cs
@@ -1,8 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 using UnityEngine.SceneManagement;
-
+using UnityEngine;
 public class main_menu : MonoBehaviour {
     public void NewGameBtn (string newGameLevel)
     {
