9abb274e6973e852bfa3e3fa88152dec20d46529
Ben M <meib1@uci.edu>
2017-12-07 23:52:06 -0800

    SideProject Done

--- TsunSim/Assets/Bird.cs
+++ TsunSim/Assets/Bird.cs
@@ -39,12 +39,12 @@ public class Bird : MonoBehaviour
     void OnCollisionEnter2D(Collision2D other)
     {
         // Zero out the bird's velocity
-        //rb2d.velocity = Vector2.zero;
+        rb2d.velocity = Vector2.zero;
         // If the bird collides with something set it to dead...
         isDead = true;
         //...tell the Animator about it...
         anim.SetTrigger("Die");
         //...and tell the game control about it.
-        //GameControl.instance.BirdDied();
+        GameControl.instance.BirdDied();
     }
 }
---
+++ TsunSim/Assets/GameControl.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class GameControl : MonoBehaviour {
+    public GameObject gameOverText;
+    public bool gameOver = false;
+    public static GameControl instance;
+	public float scrollSpeed = -1.5f;
+	public Text scoreText;
+	private int score = 0;
+	// Use this for initialization
+	void Awake ()
+    {
+		if(instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+			Destroy (gameObject);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (gameOver == true && Input.GetMouseButtonDown (0)) 
+		{
+			SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex);
+		}
+	}
+
+    public void BirdDied()
+    {
+        gameOverText.SetActive(true);
+        gameOver = true;
+    }
+
+	public void BirdScored()
+	{
+		if (gameOver) {
+			return;
+		}
+		score++;
+		scoreText.text = "score: " + score.ToString ();
+	}
+}
